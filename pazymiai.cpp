94bdf1b914712e2bb734d160d0167605af374de6
dirzinskas <mantasdirzinskas@gmail.com>
2025-09-17 17:46:28 +0300

    Add files via upload
    pirma sinchronizacija pagal reikalavimus

--- pazymiai.cpp
+++ pazymiai.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <vector>
 using namespace std;
 
 struct Studentas{
     string vardas, pavarde;
-    int nd1, nd2, nd3, egz;
+    vector<int> nd;
+    int egz;
     double galutinis;
     };
 
 int main() {
-
+    int n;
     Studentas s[3];
 
+    cout<<"iveskite nd skaiciu:"<<endl;
+    cin>>n;
+
     for(int i=0;i<3;i++){
-        cout<<"iveskite " <<i+1 <<"-ojo studento varda, pavarde, triju nd ir egzamino rezultatus:"<<endl;
-        cin>>s[i].vardas>>s[i].pavarde>>s[i].nd1>>s[i].nd2>>s[i].nd3>>s[i].egz;
-        s[i].galutinis=0.4*((s[i].nd1+s[i].nd2+s[i].nd3+s[i].egz)/3) + s[i].egz*0.6;
+        s[i].galutinis=0;
+        cout<<"iveskite " <<i+1 <<"-ojo studento varda ir pavarde"<<endl;
+        cin>>s[i].vardas>>s[i].pavarde;
+        cout<<"iveskite namu darbus:"<<endl;
+        for(int y=0;y<n;y++)
+        {
+            int x;
+            cin>>x;
+            s[i].nd.push_back(x);
+            s[i].galutinis+=x;
+        }
+        cout<<"iveskite egzamino rezultata:"<<endl;
+        cin>>s[i].egz;
+        s[i].galutinis=0.4*(s[i].galutinis/double(n))+0.6*s[i].egz;
     }
 
     
