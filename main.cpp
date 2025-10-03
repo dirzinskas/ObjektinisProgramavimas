a86f2c1e5837475796b15f7a75640ea48ac9b571
dirzinskas <mantasdirzinskas@gmail.com>
2025-10-03 23:53:54 +0300

    Add files via upload
    pridetas testavimas, tesavimo failai ( neleido kitu ikelti githubas)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include "studentas.h"
+#include <chrono>   // <-- pridėta chrono
+
+using namespace std;
+using namespace std::chrono;
 
 int main() {
     vector<Studentas> s, g, v;
@@ -12,39 +16,68 @@ int main() {
         cin >> y;
         if (cin.fail()) cin.clear();
         cin.ignore();
+
         switch (y) {
-        case 1:
+        case 1: {
+            auto start = high_resolution_clock::now();
             GeneruotiDuomenis();
+            auto end = high_resolution_clock::now();
+            cout << "Failu generavimas uztruko: "
+                 << duration_cast<milliseconds>(end - start).count()
+                 << " ms" << endl;
             break;
+        }
         case 2:
             Ivedimas(s, false);
             break;
         case 3:
             Ivedimas(s, true);
             break;
-        case 4:
+        case 4: {
             cout << "Iveskite failo varda: " << endl;
             cin >> failas;
+            auto start = high_resolution_clock::now();
             Skaitymas(s, failas);
+            auto end = high_resolution_clock::now();
+            cout << "Failo skaitymas uztruko: "
+                 << duration_cast<milliseconds>(end - start).count()
+                 << " ms" << endl;
             break;
-        case 5:
+        }
+        case 5: {
             while (true) {
                 cout << "Kokius studentus pateikti? g, v ar a?" << endl;
                 cin >> x;
                 if (x == 'g' || x == 'v' || x == 'a') break;
                 cout << "Netinkama ivestis." << endl;
             }
+            auto start = high_resolution_clock::now();
             Rusiavimas(s, g, v);
+            auto end = high_resolution_clock::now();
+            cout << "Rusiavimas i grupes uztruko: "
+                 << duration_cast<milliseconds>(end - start).count()
+                 << " ms" << endl;
+
             if (x == 'g') Rezultatai(g);
             else if (x == 'v') Rezultatai(v);
             else Rezultatai(s);
             break;
-        case 6:
+        }
+        case 6: {
             Rusiavimas(s, g, v);
-            if (g.empty() || v.empty()) cout << "Klaida. Nera tinkamu studentu" << endl;
-            Rasymas(g, "g");
-            Rasymas(v, "v");
+            if (g.empty() || v.empty()) {
+                cout << "Klaida. Nera tinkamu studentu" << endl;
+            } else {
+                auto start = high_resolution_clock::now();
+                Rasymas(g, "g");
+                Rasymas(v, "v");
+                auto end = high_resolution_clock::now();
+                cout << "Rezultatu isvedimas i failus uztruko: "
+                     << duration_cast<milliseconds>(end - start).count()
+                     << " ms" << endl;
+            }
             break;
+        }
         case 7:
             cout << "Programa baigia darba.";
             break;
@@ -54,3 +87,4 @@ int main() {
     } while (y != 7);
     return 0;
 }
+
