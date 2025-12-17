a1ce80ac92de885a04678b79a365bffef19ede33
dirzinskas <mantasdirzinskas@gmail.com>
2025-12-17 19:14:35 +0200

    perdirbtas kodas

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "studentas.h"
-#include <chrono>   // <-- pridėta chrono
+#include <chrono>
 
 using namespace std;
 using namespace std::chrono;
 
-int main() {
-    vector<Studentas> s, g, v;
+// Template function to run the menu loop for any container
+template<typename Container>
+void runProgram(Container& s, Container& g, Container& v) {
     int y;
     char x;
     string failas;
@@ -13,82 +14,106 @@ int main() {
 
     do {
         Meniu();
+
         cin >> y;
         if (cin.fail()) {
             cin.clear();
             cin.ignore(10000, '\n');
+            continue; // go back to menu
         } else {
-            cin.ignore(10000, '\n');
+            cin.ignore(10000, '\n'); // clear newline
         }
 
         switch (y) {
-        case 1: {
-            auto start = high_resolution_clock::now();
-            GeneruotiDuomenis();
-            auto end = high_resolution_clock::now();
-            cout << "Failu generavimas uztruko: "
-                 << duration_cast<milliseconds>(end - start).count()
-                 << " ms" << endl;
-            break;
-        }
-        case 2:
-            Ivedimas(s, false);
-            break;
-        case 3:
-            Ivedimas(s, true);
-            break;
-        case 4: {
-            cout << "Iveskite failo varda: " << endl;
-            cin >> failas;
-            auto start = high_resolution_clock::now();
-            Skaitymas(s, failas);
-            auto end = high_resolution_clock::now();
-            cout << "Failo skaitymas uztruko: "
-                 << duration_cast<milliseconds>(end - start).count()
-                 << " ms" << endl;
-            break;
-        }
-        case 5: {
-            while (true) {
-                cout << "Kokius studentus pateikti? g, v ar a?" << endl;
-                cin >> x;
-                if (x == 'g' || x == 'v' || x == 'a') break;
-                cout << "Netinkama ivestis." << endl;
-            }
-            auto start = high_resolution_clock::now();
-            Rusiavimas(s, g, v);
-            auto end = high_resolution_clock::now();
-            cout << "Rusiavimas i grupes uztruko: "
-                 << duration_cast<milliseconds>(end - start).count()
-                 << " ms" << endl;
-
-            if (x == 'g') Rezultatai(g);
-            else if (x == 'v') Rezultatai(v);
-            else Rezultatai(s);
-            break;
-        }
-        case 6: {
-            Rusiavimas(s, g, v);
-            if (g.empty() || v.empty()) {
-                cout << "Klaida. Nera tinkamu studentu" << endl;
-            } else {
+            case 1: {
                 auto start = high_resolution_clock::now();
-                Rasymas(g, "g");
-                Rasymas(v, "v");
+                GeneruotiDuomenis();
                 auto end = high_resolution_clock::now();
-                cout << "Rezultatu isvedimas i failus uztruko: "
+                cout << "Failu generavimas uztruko: "
                      << duration_cast<milliseconds>(end - start).count()
                      << " ms" << endl;
+                break;
             }
-            break;
+            case 2:
+                Ivedimas(s, false);
+                break;
+            case 3:
+                Ivedimas(s, true);
+                break;
+            case 4: {
+                cout << "Iveskite failo varda: ";
+                cin >> failas;
+                cin.ignore(10000, '\n');
+                auto start = high_resolution_clock::now();
+                Skaitymas(s, failas);
+                auto end = high_resolution_clock::now();
+                cout << "Failo skaitymas uztruko: "
+                     << duration_cast<milliseconds>(end - start).count()
+                     << " ms" << endl;
+                break;
+            }
+            case 5: {
+                while (true) {
+                    cout << "Kokius studentus pateikti? g, v ar a? ";
+                    cin >> x;
+                    if (x == 'g' || x == 'v' || x == 'a') break;
+                    cout << "Netinkama ivestis." << endl;
+                }
+
+                auto start = high_resolution_clock::now();
+                Rusiavimas(s, g, v);
+                auto end = high_resolution_clock::now();
+                cout << "Rusiavimas i grupes uztruko: "
+                     << duration_cast<milliseconds>(end - start).count()
+                     << " ms" << endl;
+
+                if (x == 'g') Rezultatai(g);
+                else if (x == 'v') Rezultatai(v);
+                else Rezultatai(s);
+                break;
+            }
+            case 6: {
+                Rusiavimas(s, g, v);
+                if (g.empty() || v.empty()) {
+                    cout << "Klaida. Nera tinkamu studentu" << endl;
+                } else {
+                    auto start = high_resolution_clock::now();
+                    Rasymas(g, "g");
+                    Rasymas(v, "v");
+                    auto end = high_resolution_clock::now();
+                    cout << "Rezultatu isvedimas i failus uztruko: "
+                         << duration_cast<milliseconds>(end - start).count()
+                         << " ms" << endl;
+                }
+                break;
+            }
+            case 7:
+                cout << "Programa baigia darba." << endl;
+                break;
+            default:
+                cout << "Netinkama ivestis. Pasirinkite skaiciu 1-7:" << endl;
         }
-        case 7:
-            cout << "Programa baigia darba.";
-            break;
-        default:
-            cout << "Netinkama ivestis. Pasirinkite skaiciu 1-7:" << endl;
-        }
+
     } while (y != 7);
-    return 0;
 }
 
+int main() {
+    char x;
+
+    while (true) {
+        cout << "Ar studentus laikyti list ar vector tipo konteineryje? (v ar l) ";
+        cin >> x;
+        if (x == 'v' || x == 'l') break;
+        cout << "Netinkama ivestis." << endl;
+    }
+
+    if (x == 'v') {
+        vector<Studentas> s, g, v;
+        runProgram(s, g, v); // run menu for vector
+    } else {
+        list<Studentas> s, g, v;
+        runProgram(s, g, v); // run menu for list
+    }
+
+    return 0;
+}
--- studentas.h
+++ studentas.h
@@ -11,6 +11,7 @@
 #include <ctime>
 #include <fstream>
 #include <sstream>
+#include <list>
 
 using namespace std;
 
@@ -21,16 +22,142 @@ struct Studentas {
     double vidurkis, mediana;
 };
 
+// non-template functions
 int RandomSk(int max);
 bool LyginimasStr(const Studentas &petras, const Studentas &jonas);
+void Rikiuoti(vector<Studentas>& s);
+void Rikiuoti(list<Studentas>& s);
 double Mediana(vector<int> pazymiai);
 double Vidurkis(vector<int> pazymiai);
 void GeneruotiDuomenis();
-void Skaitymas(vector<Studentas> &s, string failas);
-void Ivedimas(vector<Studentas> &s, bool generuoti);
-void Rusiavimas(vector<Studentas>& s, vector<Studentas>& g, vector<Studentas>& v);
-void Rasymas(vector<Studentas> s, string x);
-void Rezultatai(vector<Studentas> s);
 void Meniu();
+
+
+template<typename konteineris>
+void Skaitymas(konteineris& s, string failas) {
+    ifstream in(failas);
+    if (!in) {
+        cout << "Nepavyko atidaryti failo." << endl;
+        return;
+    }
+
+    string eilute;
+    getline(in, eilute);
+
+    while (getline(in, eilute)) {
+        if (eilute.empty()) continue;
+
+        istringstream iss(eilute);
+        Studentas petras;
+        iss >> petras.vardas >> petras.pavarde;
+
+        string zodis;
+        while (iss >> zodis) {
+            try {
+                petras.nd.push_back(stoi(zodis));
+            } catch (...) {
+                petras.nd.push_back(0);
+            }
+        }
+
+        if (!petras.nd.empty()) {
+            petras.egz = petras.nd.back();
+            petras.nd.pop_back();
+        }
+
+        petras.vidurkis = 0.4 * Vidurkis(petras.nd) + 0.6 * petras.egz;
+        petras.mediana  = 0.4 * Mediana(petras.nd)  + 0.6 * petras.egz;
+
+        s.push_back(petras);
+    }
+
+    cout << "Duomenys nuskaityti is failo. Viso: "
+         << s.size() << " studentu." << endl;
+}
+
+template<typename konteineris>
+void Ivedimas(konteineris& s, bool generuoti) {
+    string zodis;
+    while (true) {
+        Studentas petras;
+
+        cout << "Iveskite varda (Enter uzbaigs):" << endl;
+        getline(cin, zodis);
+        if (zodis.empty()) break;
+        petras.vardas = zodis;
+
+        cout << "Iveskite pavarde:" << endl;
+        getline(cin, zodis);
+        petras.pavarde = zodis;
+
+        if (generuoti) {
+            int ndSk = RandomSk(15);
+            for (int i = 0; i < ndSk; i++)
+                petras.nd.push_back(RandomSk(10));
+            petras.egz = RandomSk(10);
+        } else {
+            while (true) {
+                cout << "Iveskite ND pazymi (Enter uzbaigs):" << endl;
+                getline(cin, zodis);
+                if (zodis.empty()) break;
+                try {
+                    petras.nd.push_back(stoi(zodis));
+                } catch (...) {
+                    cout << "Netinkama ivestis." << endl;
+                }
+            }
+
+            cout << "Iveskite egzamino rezultata:" << endl;
+            getline(cin, zodis);
+            petras.egz = stoi(zodis);
+        }
+
+        petras.vidurkis = 0.4 * Vidurkis(petras.nd) + 0.6 * petras.egz;
+        petras.mediana  = 0.4 * Mediana(petras.nd)  + 0.6 * petras.egz;
+
+        s.push_back(petras);
+        cout << "Studento duomenys ivesti!" << endl;
+    }
+}
+
+template<typename konteineris>
+void Rusiavimas(konteineris& s, konteineris& g, konteineris& v) {
+    for (auto& st : s) {
+        if (st.vidurkis < 5 && st.mediana < 5)
+            v.push_back(st);
+        else
+            g.push_back(st);
+    }
+}
+
+template<typename konteineris>
+void Rasymas(konteineris s, string x) {
+    ofstream rez("rezultatai_" + x + ".txt");
+    rez << "Pavarde Vardas Galutinis(Vid.) Galutinis(Med.)\n";
+    for (auto& st : s)
+        rez << st.pavarde << " " << st.vardas << " "
+            << st.vidurkis << " " << st.mediana << "\n";
+}
+
+template<typename konteineris>
+void Rezultatai(konteineris s) {
+    Rikiuoti(s);
+
+    if (s.empty()) {
+        cout << "Nera ivestu studentu." << endl;
+        return;
+    }
+
+    cout << setw(20) << left << "Pavarde"
+         << setw(20) << "Vardas"
+         << setw(20) << "Galutinis (Vid.)"
+         << setw(20) << "Galutinis (Med.)" << endl;
+
+    for (auto& st : s)
+        cout << setw(20) << st.pavarde
+             << setw(20) << st.vardas
+             << setw(20) << fixed << setprecision(2) << st.vidurkis
+             << setw(20) << st.mediana << endl;
+}
 
 #endif
