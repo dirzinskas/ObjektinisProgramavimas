3a5d851c8c0b33a4617be1075fa4b8502ec6c407
dirzinskas <mantasdirzinskas@gmail.com>
2025-12-23 20:12:08 +0200

    Add files via upload

--- papildoma.cpp
+++ papildoma.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
+#include <sstream>
+#include <set>
+#include <cctype>
 using namespace std;
  
 int main(){
-string tekstas;
+string tekstas, eil;
+map<string, int> zodziai;
+map<string, set<int>> lentele;
+int eilnr=0;
 
 ifstream ivestis("zodziai.txt");
-while (getline (ivestis, tekstas)) {
-  cout<<tekstas<<endl;
+if(!ivestis)
+{
+    cout << "Failo nera" << endl;
+    return 1;
 }
-ivestis.close();
+
+while (getline(ivestis, eil)) {
+    eilnr++;
+    string zodis;
+    stringstream ss(eil);
+    while (ss >> zodis) {
+        zodziai[zodis]++;
+        lentele[zodis].insert(eilnr);
+    }
+}
+cout<< "Zodziu skaicius faile: " << zodziai.size() << endl;
 }
