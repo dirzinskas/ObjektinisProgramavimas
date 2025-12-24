b047aa905d159ed3aaddf77b202cdb5bc20e7b9e
dirzinskas <mantasdirzinskas@gmail.com>
2025-12-24 14:01:09 +0200

    Add files via upload

---
+++ url.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+ 
+int main(){
+string tekstas, eil, visas;
+
+cout<<"Iveskite failo pavadinima: ";
+cin>>tekstas;
+
+ifstream ivestis(tekstas);
+if(!ivestis)
+{
+    cout << "Failo nera" << endl;
+    return 1;
+}
+
+while (getline(ivestis, eil)) {
+    string zodis;
+    stringstream ss(eil);
+    while (ss >> zodis) { 
+        if(zodis.find("http")==0 || zodis.find("www.")==0){
+            visas +=zodis + "\n";
+        }
+    }
+}
+ofstream rez("rezultatasurl.txt");
+
+rez << visas;
+return 0;
+}
